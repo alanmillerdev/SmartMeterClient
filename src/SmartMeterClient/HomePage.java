969851a04a66c7e552aa67c5223e9ec5901dbbd5
Alan <alanmiller123456789@gmail.com>
2023-02-05 16:10:11 +0000

    Commented out error

--- src/SmartMeterClient/HomePage.java
+++ src/SmartMeterClient/HomePage.java
@@ -153,10 +153,12 @@ public class HomePage extends JFrame {
 	}
 
 	// Error caused here when trying to draw the border
+	/*
 	public void paint(Graphics g) {
 		Graphics2D g2d = (Graphics2D) g;
 		g2d.setStroke(new BasicStroke(3));
 		g2d.drawRoundRect(20, 40, 1360, 740, 50, 30);
 		g2d.drawRoundRect(40, 60, 1320, 650, 50, 30);
 	}
+	*/
 }
